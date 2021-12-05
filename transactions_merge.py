b559c7a434ed0a24627da3e6823ff4d770d433f6
nunosanto <up201405774@up.pt>
2021-12-05 02:57:10 +0000

    added info on transactions

--- transactions_merge.py
+++ transactions_merge.py
@@ -27,26 +27,58 @@ df_trans = pd.read_csv("D:/FEUP/AC/AC/processed_files/trans_train_processed.csv"
 merged=df_trans[['account_id']]
 merged=merged.drop_duplicates(subset=['account_id'])
 
-credit_count=df_trans.groupby('account_id')['type'].apply(lambda x: (x=='credit').sum()).reset_index(name='credit count')
-withdrawal_count=df_trans.groupby('account_id')['type'].apply(lambda x: (x=='withdrawal').sum()).reset_index(name='withdrawl count')
-amount_std=df_trans.groupby('account_id')['amount'].std().reset_index(name='amount std')
-#actions=df_trans['account_id',df_trans['type']=='withdrawl'].groupby['account_id'].count()
-balancemean=df_trans[['account_id', 'balance']].groupby('account_id').mean()
-balancemin=df_trans[['account_id', 'balance']].groupby('account_id').min()
-balancemax=df_trans[['account_id', 'balance']].groupby('account_id').max()
+#credit_count=df_trans.groupby('account_id')['type'].apply(lambda x: (x=='credit').sum()).reset_index(name='credit count')
+#withdrawal_count=df_trans.groupby('account_id')['type'].apply(lambda x: (x=='withdrawal').sum()).reset_index(name='withdrawl count')
 
-meantrans = df_trans[['account_id', 'amount']].groupby('account_id').mean()
-maxtrans = df_trans[['account_id', 'amount']].groupby('account_id').max()
-mintrans= df_trans[['account_id', 'amount']].groupby('account_id').min()
+# operation on dataset with CREDIT
+
+credit_df=df_trans.loc[df_trans['type']=='credit']
+credit_count=credit_df.groupby('account_id')['type'].count().reset_index(name='credit count')
+credit_balancemean=credit_df.groupby('account_id')['balance'].mean().round().reset_index(name='credit balance mean')
+credit_balancemin=credit_df.groupby('account_id')['balance'].min().round().reset_index(name='credit balance min')
+credit_balancemax=credit_df.groupby('account_id')['balance'].max().round().reset_index(name='credit balance max')
+
+credit_amountmean = credit_df.groupby('account_id')['amount'].mean().round().reset_index(name='credit amount mean')
+credit_amountmin = credit_df.groupby('account_id')['amount'].min().round().reset_index(name='credit amount min')
+credit_amountmax= credit_df.groupby('account_id')['amount'].max().round().reset_index(name='credit amount max')
+credit_amount_std=credit_df.groupby('account_id')['amount'].std().round().reset_index(name='credit amount std')
+
+
+# operation on dataset with WITHDRAWAL
+
+withdrawal_df=df_trans.loc[df_trans['type']=='withdrawal']
+withdrawal_count=withdrawal_df.groupby('account_id')['type'].count().reset_index(name='withdrawal count')
+withdrawal_balancemean=withdrawal_df.groupby('account_id')['balance'].mean().round().reset_index(name='withdrawal balance mean')
+withdrawal_balancemin=withdrawal_df.groupby('account_id')['balance'].min().round().reset_index(name='withdrawal balance min')
+withdrawal_balancemax=withdrawal_df.groupby('account_id')['balance'].max().round().reset_index(name='withdrawal balance max')
+
+withdrawal_amountmean = withdrawal_df.groupby('account_id')['amount'].mean().round().reset_index(name='withdrawal amount mean')
+withdrawal_amountmin = withdrawal_df.groupby('account_id')['amount'].min().round().reset_index(name='withdrawal amount min')
+withdrawal_amountmax= withdrawal_df.groupby('account_id')['amount'].max().round().reset_index(name='withdrawal amount max')
+withdrawal_amount_std=withdrawal_df.groupby('account_id')['amount'].std().round().reset_index(name='withdrawal amount std')
+
+
+
+# merge on CREDIT
+
 merged=merged.merge(credit_count,on='account_id')
+merged=merged.merge(credit_balancemean,on='account_id')
+merged=merged.merge(credit_balancemin,on='account_id')
+merged=merged.merge(credit_balancemax,on='account_id')
+merged = merged.merge(credit_amountmean,on='account_id')
+merged = merged.merge(credit_amountmin,on='account_id')
+merged=merged.merge(credit_amountmax,on='account_id')
+merged=merged.merge(credit_amount_std,on='account_id')
+
+# merge on WITHDRAWAL
+
 merged=merged.merge(withdrawal_count,on='account_id')
-merged=merged.merge(balancemean,on='account_id')
-merged=merged.merge(balancemin,on='account_id')
-merged=merged.merge(balancemax,on='account_id')
-merged = merged.merge(meantrans,on='account_id')
-merged = merged.merge(maxtrans,on='account_id')
-merged=merged.merge(mintrans,on='account_id')
-merged=merged.merge(amount_std.round(),on='account_id')
-merged = merged.rename(columns={'type':'number of actions','amount_x':'transaction mean','amount_y':'transaction max','amount' : 'transaction min', 'balance_x': 'balance mean','balance_y':'balance min','balance':'balance max'})
+merged=merged.merge(withdrawal_balancemean,on='account_id')
+merged=merged.merge(withdrawal_balancemin,on='account_id')
+merged=merged.merge(withdrawal_balancemax,on='account_id')
+merged = merged.merge(withdrawal_amountmean,on='account_id')
+merged = merged.merge(withdrawal_amountmin,on='account_id')
+merged=merged.merge(withdrawal_amountmax,on='account_id')
+merged=merged.merge(withdrawal_amount_std,on='account_id')
 
 merged.to_csv("D:/FEUP/AC/AC/testdataset.csv",index=False)
