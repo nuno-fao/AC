150c0d3b05e5613ac637a75d39dc18cc44d2a67d
nunosanto <up201405774@up.pt>
2021-12-06 01:57:00 +0000

    operation info added

--- transactions_merge.py
+++ transactions_merge.py
@@ -2,33 +2,10 @@ import pandas as pd
 
 df_trans = pd.read_csv("D:/FEUP/AC/AC/processed_files/trans_train_processed.csv",sep=',')
 
-# def add_value(col,id,value):
-
-#     return
-
-# def merge_accounts():
-#     global df_client
-#     global df_account
-#     global df_disp
-
-#     df_client["account_id"]=""
-#     df_client["account_district"]=""
-#     df_client["disp_type"]=""
-#     df_client["account_frequency"]=""
-#     df_client["account_creation"]=""
-
-#     for i,row in df_disp.iterrows():
-#         print(df_account.loc[df_account['account_id'] == row['account_id']].items())
-#         return
-
-# merge_accounts()
-
 
 merged=df_trans[['account_id']]
 merged=merged.drop_duplicates(subset=['account_id'])
 
-#credit_count=df_trans.groupby('account_id')['type'].apply(lambda x: (x=='credit').sum()).reset_index(name='credit count')
-#withdrawal_count=df_trans.groupby('account_id')['type'].apply(lambda x: (x=='withdrawal').sum()).reset_index(name='withdrawl count')
 
 # operation on dataset with CREDIT
 
@@ -59,6 +36,30 @@ withdrawal_amount_std=withdrawal_df.groupby('account_id')['amount'].std().round(
 
 
 
+# operation on dataset with OPERATION == 1
+operationA_df=df_trans.loc[df_trans['operation']==1]
+operationA_amountmean = operationA_df.groupby('account_id')['amount'].mean().round().reset_index(name='OPERATION A amount mean')
+
+# operation on dataset with OPERATOR == 2
+operationB_df=df_trans.loc[df_trans['operation']==2]
+operationB_amountmean = operationB_df.groupby('account_id')['amount'].mean().round().reset_index(name='OPERATION B amount mean')
+
+# operation on dataset with OPERATOR == 3
+operationC_df=df_trans.loc[df_trans['operation']==3]
+operationC_amountmean = operationC_df.groupby('account_id')['amount'].mean().round().reset_index(name='OPERATION C amount mean')
+
+# operation on dataset with OPERATOR == 4
+operationD_df=df_trans.loc[df_trans['operation']==4]
+operationD_amountmean = operationD_df.groupby('account_id')['amount'].mean().round().reset_index(name='OPERATION D amount mean')
+
+# operation on dataset with OPERATOR == 5
+operationE_df=df_trans.loc[df_trans['operation']==5]
+operationE_amountmean = operationE_df.groupby('account_id')['amount'].mean().round().reset_index(name='OPERATION E amount mean')
+
+# operation on dataset with OPERATOR == 6
+operationF_df=df_trans.loc[df_trans['operation']==6]
+operationF_amountmean = operationF_df.groupby('account_id')['amount'].mean().round().reset_index(name='OPERATION F amount mean')
+
 # merge on CREDIT
 
 merged=merged.merge(credit_count,on='account_id')
@@ -80,5 +81,16 @@ merged = merged.merge(withdrawal_amountmean,on='account_id')
 merged = merged.merge(withdrawal_amountmin,on='account_id')
 merged=merged.merge(withdrawal_amountmax,on='account_id')
 merged=merged.merge(withdrawal_amount_std,on='account_id')
+
+# merge on OPERATION TYPE
+
+merged=merged.merge(operationA_amountmean,on='account_id')
+merged=merged.merge(operationB_amountmean,on='account_id')
+merged=merged.merge(operationC_amountmean,on='account_id')
+merged=merged.merge(operationD_amountmean,on='account_id')
+merged=merged.merge(operationE_amountmean,on='account_id')
+merged=merged.merge(operationF_amountmean,on='account_id')
+
+
 
 merged.to_csv("D:/FEUP/AC/AC/testdataset.csv",index=False)
