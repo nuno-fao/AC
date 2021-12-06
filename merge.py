41ffe105d782fc084a88a9133e9658a97937e684
nunosanto <up201405774@up.pt>
2021-12-06 20:18:37 +0000

    problem with merge fixed

--- merge.py
+++ merge.py
@@ -49,57 +49,77 @@ def transaction_stats(df_trans):
     # operation on dataset with OPERATION == 1
     operationA_df=df_trans.loc[df_trans['operation']==1]
     operationA_amountmean = operationA_df.groupby('account_id')['amount'].mean().round().reset_index(name='OPERATION A amount mean')
+    operationA_count = operationA_df.groupby('account_id')['account_id'].count().reset_index(name='OPERATION A count')
 
     # operation on dataset with OPERATOR == 2
     operationB_df=df_trans.loc[df_trans['operation']==2]
     operationB_amountmean = operationB_df.groupby('account_id')['amount'].mean().round().reset_index(name='OPERATION B amount mean')
+    operationB_count = operationB_df.groupby('account_id')['account_id'].count().reset_index(name='OPERATION B count')
 
     # operation on dataset with OPERATOR == 3
     operationC_df=df_trans.loc[df_trans['operation']==3]
     operationC_amountmean = operationC_df.groupby('account_id')['amount'].mean().round().reset_index(name='OPERATION C amount mean')
+    operationC_count = operationC_df.groupby('account_id')['account_id'].count().reset_index(name='OPERATION C count')
 
     # operation on dataset with OPERATOR == 4
     operationD_df=df_trans.loc[df_trans['operation']==4]
     operationD_amountmean = operationD_df.groupby('account_id')['amount'].mean().round().reset_index(name='OPERATION D amount mean')
+    operationD_count = operationD_df.groupby('account_id')['account_id'].count().reset_index(name='OPERATION D count')
 
     # operation on dataset with OPERATOR == 5
     operationE_df=df_trans.loc[df_trans['operation']==5]
     operationE_amountmean = operationE_df.groupby('account_id')['amount'].mean().round().reset_index(name='OPERATION E amount mean')
+    operationE_count = operationE_df.groupby('account_id')['account_id'].count().reset_index(name='OPERATION E count')
 
     # operation on dataset with OPERATOR == 6
     operationF_df=df_trans.loc[df_trans['operation']==6]
     operationF_amountmean = operationF_df.groupby('account_id')['amount'].mean().round().reset_index(name='OPERATION F amount mean')
+    operationF_count = operationF_df.groupby('account_id')['account_id'].count().reset_index(name='OPERATION F count')
 
     # merge on CREDIT
 
-    merged=merged.merge(credit_count,on='account_id')
-    merged=merged.merge(credit_balancemean,on='account_id')
-    merged=merged.merge(credit_balancemin,on='account_id')
-    merged=merged.merge(credit_balancemax,on='account_id')
-    merged = merged.merge(credit_amountmean,on='account_id')
-    merged = merged.merge(credit_amountmin,on='account_id')
-    merged=merged.merge(credit_amountmax,on='account_id')
-    merged=merged.merge(credit_amount_std,on='account_id')
+    merged=merged.merge(credit_count,how='left',on='account_id')
+    merged=merged.merge(credit_balancemean,how='left',on='account_id')
+    merged=merged.merge(credit_balancemin,how='left',on='account_id')
+    merged=merged.merge(credit_balancemax,how='left',on='account_id')
+    merged = merged.merge(credit_amountmean,how='left',on='account_id')
+    merged = merged.merge(credit_amountmin,how='left',on='account_id')
+    merged=merged.merge(credit_amountmax,how='left',on='account_id')
+    merged=merged.merge(credit_amount_std,how='left',on='account_id')
 
     # merge on WITHDRAWAL
 
-    merged=merged.merge(withdrawal_count,on='account_id')
-    merged=merged.merge(withdrawal_balancemean,on='account_id')
-    merged=merged.merge(withdrawal_balancemin,on='account_id')
-    merged=merged.merge(withdrawal_balancemax,on='account_id')
-    merged = merged.merge(withdrawal_amountmean,on='account_id')
-    merged = merged.merge(withdrawal_amountmin,on='account_id')
-    merged=merged.merge(withdrawal_amountmax,on='account_id')
-    merged=merged.merge(withdrawal_amount_std,on='account_id')
+    merged=merged.merge(withdrawal_count,how='left',on='account_id')
+    merged=merged.merge(withdrawal_balancemean,how='left',on='account_id')
+    merged=merged.merge(withdrawal_balancemin,how='left',on='account_id')
+    merged=merged.merge(withdrawal_balancemax,how='left',on='account_id')
+    merged = merged.merge(withdrawal_amountmean,how='left',on='account_id')
+    merged = merged.merge(withdrawal_amountmin,how='left',on='account_id')
+    merged=merged.merge(withdrawal_amountmax,how='left',on='account_id')
+    merged=merged.merge(withdrawal_amount_std,how='left',on='account_id')
 
     # merge on OPERATION TYPE
 
-    merged=merged.merge(operationA_amountmean,on='account_id')
-    merged=merged.merge(operationB_amountmean,on='account_id')
-    merged=merged.merge(operationC_amountmean,on='account_id')
-    merged=merged.merge(operationD_amountmean,on='account_id')
-    merged=merged.merge(operationE_amountmean,on='account_id')
-    merged=merged.merge(operationF_amountmean,on='account_id')
+    merged=merged.merge(operationA_amountmean,how='left',on='account_id')
+    merged=merged.merge(operationA_count,how='left',on='account_id')
+
+    merged=merged.merge(operationB_amountmean,how='left',on='account_id')
+    merged=merged.merge(operationB_count,how='left',on='account_id')
+
+    merged=merged.merge(operationC_amountmean,how='left',on='account_id')
+    merged=merged.merge(operationC_count,how='left',on='account_id')
+
+    merged=merged.merge(operationD_amountmean,how='left',on='account_id')
+    merged=merged.merge(operationD_count,how='left',on='account_id')
+
+    merged=merged.merge(operationE_amountmean,how='left',on='account_id')
+    merged=merged.merge(operationE_count,how='left',on='account_id')
+
+    merged=merged.merge(operationF_amountmean,how='left',on='account_id')
+    merged=merged.merge(operationF_count,how='left',on='account_id')
+
+
+    merged=merged.fillna(0)
     return merged
 
 def merge_datasets(clients,cards,accounts,disps,districts,loans,trans,final_name):
